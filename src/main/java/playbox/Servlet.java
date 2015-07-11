c56ff23983c9f2fd1138d9b6a841378e49aafbce
Андрей <@192.168.0.142>
2015-07-11 23:15:53 +0300

    #11 complited

--- src/main/java/playbox/Servlet.java
+++ src/main/java/playbox/Servlet.java
@@ -5,6 +5,7 @@
  */
 package playbox;
 
+import edu.roi.playbox.domain.User;
 import edu.roi.playbox.domain.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
@@ -36,13 +37,33 @@ public class Servlet extends HttpServlet {
         resp.setContentType("text/html;charset=utf-8");
 
         PrintWriter pw = resp.getWriter();
-        pw.println("<head> meta charset=\"UTF-8\"</head>");
+        
+        pw.println("<html>");
+        pw.println("<head> <meta charset=\"UTF-8\"></head>");
         pw.println("<body>");
-        pw.println("<B>Список групп</B>");
-        pw.println("<table border=1>");
+        pw.println("<table>");
+        pw.println("<tr>");
+        pw.println("<th>Имя</th>");
+        pw.println("<th>Фамилия</th>");
+        pw.println("<th>Skype</th>");
+        pw.println("<th>Github</th>");
+        pw.println("<th>О себе</th>");
+        pw.println("<tr>");
+        
+        List userList = userDao.findAll();
+        for(int i = 0 ; i< userList.size();i++){
+            User user = (User) userList.get(i);
+            pw.println("<tr>");
+            pw.println("<td>"+user.getFirstName()+"</td>");
+            pw.println("<td>"+user.getLastName()+"</td>");
+            pw.println("<td>"+user.getSkypeLogin()+"</td>");
+            pw.println("<td>"+user.getGithubLogin()+"</td>");
+            pw.println("<td>"+user.getDescription()+"</td>");
+            pw.println("</tr>");
+        }
         pw.println("</table>");
         pw.println("</body>");
-
+        pw.println("</html>");
     }
 
     public void setUserDao(UserDao userDao) {
