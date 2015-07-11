800651d2657468c811e80458c195be0f94b7e924
Alexey Pavelchuk <alex.pavelchuk@gmail.com>
2015-07-11 15:59:14 +0300

    #11

--- src/main/resources/sql/db-user-data.sql
+++ src/main/resources/sql/db-user-data.sql
@@ -1,1 +1,5 @@
-INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Alexey', 'Pavelchuk', 'AlexRP239', 'AlexRP239', 'alex.pavelchuk@gmail.com')
+INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Alexey', 'Pavelchuk', 'AlexRP239', 'AlexRP239', 'alex.pavelchuk@gmail.com');
+INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Andrey', 'Larionov', 'Peepsi', 'larik1994', 'unknown');
+INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Alexandr', 'Yurgilevich', 'Yurgil', 'yurgil_viper', 'unknown');
+INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Andrey', 'Yakovlev', 'Darkenery', 'iioqpuruc13', 'unknown');
+-- and others todo: Victoria
