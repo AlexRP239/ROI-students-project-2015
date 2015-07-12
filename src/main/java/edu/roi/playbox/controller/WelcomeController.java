eeaf387ddb82b50025d152c38811b6b8f6c1fe7c
Alexey Pavelchuk <alex.pavelchuk@gmail.com>
2015-07-13 00:17:57 +0300

    More complex example - implement credentials servlet using spring mvc

--- src/main/java/edu/roi/playbox/controller/WelcomeController.java
+++ src/main/java/edu/roi/playbox/controller/WelcomeController.java
@@ -1,8 +1,11 @@
 package edu.roi.playbox.controller;
 
+import edu.roi.playbox.domain.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -12,6 +15,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Controller
 public class WelcomeController {
 
+    @Autowired
+    private UserDao userDao;
+
     // Если внутри сервисного метода (singleton) хранится состояние, то данные должны быть должным образом сихнронизированы
     // Просто private int visitorCount; visitorCouтt++ не подходит, потому что операция ++ не атомарная
     private final AtomicInteger visitorCount = new AtomicInteger(0);
@@ -20,5 +26,16 @@ public class WelcomeController {
     public String welcome(Model model) {
         model.addAttribute("visitorCount", visitorCount.incrementAndGet());
         return "welcome"; // /WEB-INF/templates/welcome.html
+    }
+
+    @RequestMapping("mvc/credentials")
+    public ModelAndView credentials() {
+        final ModelAndView modelAndView = new ModelAndView("credentials"); // /WEB-INF/templates/credentials.html
+        modelAndView.addObject("users", userDao.findAll());
+        return modelAndView;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
     }
 }
