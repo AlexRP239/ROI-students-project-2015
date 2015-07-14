dee48dc19d23617a333f1ec3fd9ea5ecc103a4d0
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2015-07-14 11:58:05 +0300

    add to DB

--- src/main/resources/sql/db-user-data.sql
+++ src/main/resources/sql/db-user-data.sql
@@ -1,5 +1,6 @@
 INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Alexey', 'Pavelchuk', 'AlexRP239', 'AlexRP239', 'alex.pavelchuk@gmail.com');
 INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Andrey', 'Larionov', 'Peepsi', 'larik1994', 'unknown');
 INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Alexandr', 'Yurgilevich', 'Yurgil', 'yurgil_viper', 'unknown');
-INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Andrey', 'Yakovlev', 'Darkenery', 'iioqpuruc13', 'unknown');
+INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Artem', 'Yakovlev', 'Darkenery', 'iioqpuruc13', 'unknown');
+INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Igor', 'Nikolaev', 'karlson35', 'karlson35rus', 'nikolaev.spbstu@gmail.com');
 -- and others todo: Victoria
