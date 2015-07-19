252d68dc696806d08f83672e9b902cf73bd9e50a
olezhek28 <olezhek2804@gmail.com>
2015-07-19 16:57:03 +0300

    #12

--- src/main/java/edu/roi/playbox/domain/DestinationAccount.java
+++ src/main/java/edu/roi/playbox/domain/DestinationAccount.java
@@ -1,7 +1,72 @@
 package edu.roi.playbox.domain;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
 /**
  * Created by karlson35 on 19.07.2015.
  */
+
+@Entity
+@Table(name = "DestinationAccount")
+
+
 public class DestinationAccount {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+    // @Column(name = "paymentMethod")
+    //private PaymentMethod paymentMethod;
+    @Column(name = "amount",precision = 10, scale = 2)
+    private BigDecimal amount;
+    @Column(name = "maxAccountAmount",precision = 10, scale = 2)
+    private BigDecimal maxAccountAmount;
+    @Column(name = "maxPaymentAmount",precision = 10, scale = 2)
+    private BigDecimal maxPaymentAmount;
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public BigDecimal getMaxPaymentAmount() {
+        return maxPaymentAmount;
+    }
+
+    public void setMaxPaymentAmount(BigDecimal maxPaymentAmount) {
+        this.maxPaymentAmount = maxPaymentAmount;
+    }
+
+    public BigDecimal getMaxAccountAmount() {
+        return maxAccountAmount;
+    }
+
+    public void setMaxAccountAmount(BigDecimal maxAccountAmount) {
+        this.maxAccountAmount = maxAccountAmount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
 }
