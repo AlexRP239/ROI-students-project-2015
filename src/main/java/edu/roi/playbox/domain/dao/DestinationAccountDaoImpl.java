d4e819da1772da534c1ae3e75887d3a0b9e4f606
olezhek28 <olezhek2804@gmail.com>
2015-07-20 19:45:51 +0300

    #13

---
+++ src/main/java/edu/roi/playbox/domain/dao/DestinationAccountDaoImpl.java
@@ -0,0 +1,35 @@
+package edu.roi.playbox.domain.dao;
+
+import edu.roi.playbox.domain.DestinationAccount;
+import edu.roi.playbox.domain.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * Created by olezhek on 20.07.2015.
+ */
+
+
+
+public class DestinationAccountDaoImpl implements DestinationAccountDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public DestinationAccount saveOrUpdate(DestinationAccount customer) {
+        if (customer.getId() == null) {
+            em.persist(customer);
+        } else {
+            customer = em.merge(customer);
+        }
+        return customer;
+    }
+
+    @Override
+    public List<DestinationAccount> findEnabled() {
+        return null;
+    }
+}
