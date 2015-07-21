92f77e879750d77f1c6bc5471cf6e320485662c9
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2015-07-21 11:04:05 +0300

    UserDatabase changes

--- src/main/resources/sql/db-user-data.sql
+++ src/main/resources/sql/db-user-data.sql
@@ -4,4 +4,5 @@ INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('A
 INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Artem', 'Yakovlev', 'Darkenery', 'iioqpuruc13', 'unknown');
 INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Igor', 'Nikolaev', 'karlson35', 'karlson35rus', 'nikolaev.spbstu@gmail.com');
 INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Danila', 'Lapko', 'danlapko', 'danlapko', 'danlapko@yandex.ru');
+INSERT INTO USER(FIRSTNAME, LASTNAME, GITHUBLOGIN, SKYPELOGIN, EMAIL) VALUES ('Nikita', 'Koshmak', 'KoshmakNikita', 'shkedovic', 'unknown');
 -- and others todo: Victoria
