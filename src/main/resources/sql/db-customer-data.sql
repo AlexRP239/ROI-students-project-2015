73e2d9363c83896f0718db68ea0c49f74a19ca9d
Igor Nikolaev <nikolaev.spbstu@gmail.com>
2015-07-21 10:53:05 +0300

    database changes

---
+++ src/main/resources/sql/db-customer-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOMER (businessName,secretKey,contact,blocked) VALUES ('Vasya','secretpw','contactVasi',false) ;
+INSERT INTO CUSTOMER (businessName,secretKey,contact,blocked) VALUES ('Vitek','secretKeY','contactViti',false) ;
+INSERT INTO CUSTOMER (businessName,secretKey,contact,blocked) VALUES ('Zhenek','supersecretpw','contactZheni',true) ;
+INSERT INTO CUSTOMER (businessName,secretKey,contact,blocked) VALUES ('Vanya','supermegasecretpw','contactVani',false) ;
---
+++ src/main/resources/sql/db-destinationAccount-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO DestinationAccount (amount, maxAccountAmount, maxPaymentAmount, enabled) VALUES ('12000.83','10000000.01','100000.03',TRUE);
+INSERT INTO DestinationAccount (amount, maxAccountAmount, maxPaymentAmount, enabled) VALUES ('120.68','10000000.01','100000.03',TRUE);
+INSERT INTO DestinationAccount (amount, maxAccountAmount, maxPaymentAmount, enabled) VALUES ('0.34','10000000.01','100000.03',TRUE);
+INSERT INTO DestinationAccount (amount, maxAccountAmount, maxPaymentAmount, enabled) VALUES ('130007.74','10000000.01','100000.03',FALSE);
+INSERT INTO DestinationAccount (amount, maxAccountAmount, maxPaymentAmount, enabled) VALUES ('87900.84','10000000.01','100000.03',TRUE);
