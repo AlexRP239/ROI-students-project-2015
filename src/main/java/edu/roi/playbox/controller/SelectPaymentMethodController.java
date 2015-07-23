81fea4cd7e25435c5c845d16f83b865e09283804
Iprogram2hours <a.o.nechaev.95@gmail.com>
2015-07-23 10:22:58 +0300

    la la la

--- src/main/java/edu/roi/playbox/controller/SelectPaymentMethodController.java
+++ src/main/java/edu/roi/playbox/controller/SelectPaymentMethodController.java
@@ -1,22 +1,27 @@
 package edu.roi.playbox.controller;
 
+import edu.roi.playbox.domain.Customer;
+import edu.roi.playbox.domain.dao.CustomerDao;
+import org.hibernate.jpa.criteria.expression.function.CurrentDateFunction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * Created by AlexRP239 on 19.07.2015.
  */
 @Controller
 public class SelectPaymentMethodController {
+
+    @Autowired
+    private CustomerDao CustomerDao;
 
     // todo: inject PaymentDao, CustomerDao, DestinationAccountDao beans (не знаю как правильно по-русски inject перевести - вставить из контекста :)
 
@@ -33,10 +38,22 @@ public class SelectPaymentMethodController {
     // пример как передавать параметры через адресную строку
     @RequestMapping("example/{customerId}")
     @ResponseBody
-    public String parseParametersExample(
+    public void parseParametersExample(
             @RequestParam("secretKey") String secretKey,
             @RequestParam("invoiceId") Long invoiceId,
-            @RequestParam("amount") BigDecimal amount) {
-        return "secretKey=" + secretKey + " invoiceId=" + invoiceId + " amount=" + amount;
+            @RequestParam("amount") BigDecimal amount,
+            @PathVariable("customerId") Long customerId,
+            HttpServletResponse response) throws IOException {
+        //Проверка CustomerDao
+        Customer customerExample = CustomerDao.findById(customerId);
+        if (customerExample == null) response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access denied");
+        else if (customerExample.getSecretKey() != secretKey ||
+                customerExample.getExpired().before(new Date()) == true ||
+                customerExample.getBlocked() == true) response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access denied");
+        Date d = new Date();
+        //Проверка PaymentDao
+
+
+
     }
 }
